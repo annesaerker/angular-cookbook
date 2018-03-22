a2777991fff0d68ae3163ea15eacc202f7df8dda
Anne Saerker <annemai11@hotmail.com>
2018-03-22 11:54:58 +0100

    routing

--- angular-cookbook/src/app/app.module.ts
+++ angular-cookbook/src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { AllRecipesComponent } from './all-recipes/all-recipes.component';
+import { AdminComponent } from './admin/admin.component';
+import { MyRecipesComponent } from './admin/my-recipes/my-recipes.component';
+import { MyProfileComponent } from './admin/my-profile/my-profile.component';
+import { MyFavoritesComponent } from './admin/my-favorites/my-favorites.component';
+import { AllComponent } from './all/all.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AddRecipeComponent,
+    AllRecipesComponent,
+    AdminComponent,
+    MyRecipesComponent,
+    MyProfileComponent,
+    MyFavoritesComponent,
+    AllComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    [MatButtonModule, MatCheckboxModule]
+    [MatButtonModule, MatCheckboxModule, MatToolbarModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
